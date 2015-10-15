6bb7cf281a493408e016c7b56ede893e24562acf
amyjoscelyn <amy.joscelyn@flatironschool.com>
2015-10-15 13:47:42 -0400

    made single view of Summary screen

---
+++ DailyBudgetTracker/DailyBudgetTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DailyBudgetTracker
+//
+//  Created by Amy Joscelyn on 10/15/15.
+//  Copyright © 2015 Amy Joscelyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DailyBudgetTracker/DailyBudgetTracker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DailyBudgetTracker
+//
+//  Created by Amy Joscelyn on 10/15/15.
+//  Copyright © 2015 Amy Joscelyn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
